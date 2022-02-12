653072164da6f4f91bbb3e366a306b64f0f9b7ad
Tarun Sabharwal <tarunsbh@gmail.com>
2022-02-12 18:57:03 +0530

    Update Main.java

--- GLassesment/Glassisment/glassignment/src/Main.java
+++ GLassesment/Glassisment/glassignment/src/Main.java
@@ -39,7 +39,7 @@ Scanner sc = new Scanner(System.in);
     	   int n;
     	   System.out.println("Enter a number: ");
     	   n = sc.nextInt();
-    	   for(int j=1; j<n ;j++) {
+    	   for(int j=1; j<=n ;j++) {
     		   for (int k=n; k>=j; k--)
     			   System.out.print("*");
     		   System.out.println();
